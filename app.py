9762a6dd1d6962548efc29571ecd69951fb2689d
DavidQiu <suzukiami607@gmail.com>
2023-09-23 18:08:45 +0800

    test remove headless

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ options.add_argument("start-maximized")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-extensions')
-options.add_argument('--headless')
 driver = uc.Chrome(options=options)
 
 @app.route('/')
